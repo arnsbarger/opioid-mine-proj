deb1fb6c1bc1339a5c5c576ee88d154d434474aa
Madison Arnsbarger <maddieka@vt.edu>
2020-01-23 15:01:37 -0500

    removed exploratory stuff; corrected class of variables

--- 00-clean-cdc.R
+++ 00-clean-cdc.R
@@ -17,45 +17,14 @@ cdc_deaths10_17 <- read.delim("Query2010-2017.txt")
 cdc <- rbind(cdc_deaths1999,cdc_deaths2000,cdc_deaths01_02,cdc_deaths03_05,cdc_deaths06_09,cdc_deaths10_17)
 rm(cdc_deaths1999,cdc_deaths2000,cdc_deaths01_02,cdc_deaths03_05,cdc_deaths06_09,cdc_deaths10_17)
 
-#missing <- c("Suppressed","Missing","Unreliable")
+cdc$Crude.Rate <- as.numeric(as.character(cdc$Crude.Rate))
+cdc$Year <- as.numeric(as.character(cdc$Year))
+cdc$Crude.Rate.Standard.Error <- as.numeric(as.character(cdc$Crude.Rate.Standard.Error))
+cdc$Crude.Rate.Lower.95..Confidence.Interval <- as.numeric(as.character(cdc$Crude.Rate.Lower.95..Confidence.Interval))
 
-table(cdc$Deaths) # 46243 suppressed, 116 missing
-plot(density(as.numeric(cdc$Deaths), na.rm = TRUE)) # why is there such a spike around 100?
+cdc_na <- cdc
+cdc_na[cdc_na == "Suppressed" | cdc_na == "Missing" | cdc_na == "Unreliable"] <- NA
 
-cdc$State <- str_sub(cdc$County, -2,-1)
-cdc$State.Code <- str_sub(as.character(cdc$County.Code), 1, 2)
-
-table(cdc$Year[cdc$Deaths=="Suppressed"], cdc$Year[cdc$Deaths=="Suppressed"])
-
-cdc_deaths[cdc_deaths == "Suppressed" | cdc_deaths == "Missing" | cdc_deaths == "Unreliable"] <- NA
-cdc_deaths <- cdc_deaths %>% filter(UCD...Drug.Alcohol.Induced.Code == "D")
-
-sum(!is.na(cdc_deaths$Crude.Rate))/nrow(cdc_deaths) # only 12.6% of data is complete :(
-
-for (i in 1:ncol(cdc_deaths)) {
-    print(class(cdc_deaths[,i]))
-}
-
-cdc_deaths$Crude.Rate <- as.numeric(as.character(cdc_deaths$Crude.Rate))
-cdc_deaths$Year <- as.numeric(as.character(cdc_deaths$Year))
-cdc_deaths$Crude.Rate.Standard.Error <- as.numeric(as.character(cdc_deaths$Crude.Rate.Standard.Error))
-cdc_deaths$Crude.Rate.Lower.95..Confidence.Interval <- as.numeric(as.character(cdc_deaths$Crude.Rate.Lower.95..Confidence.Interval))
-
-
-sum(cdc_deaths$Crude.Rate>0, na.rm = TRUE) # only 7573 observations across 18 years
-
-years <- data.frame(year = unique(cdc_deaths$Year), count = NA, percent_country = NA)
-
-for (i in years$year) {
-    years$count[i-1998] <- sum(cdc_deaths$Crude.Rate[cdc_deaths$Year == i] > 0, na.rm = TRUE)
-    years$percent_country[i-1998] <- years$count[i-1998]/length(unique(cdc_deaths$County[cdc_deaths$Year == i]))
-}
-
-# between 5-20% of the country is represented in each year
-
-min(cdc_deaths$Crude.Rate, na.rm = TRUE)
-plot(density(cdc_deaths$Crude.Rate.Standard.Error[is.na(cdc_deaths$Crude.Rate)], na.rm = TRUE))
-plot(density(cdc_deaths$Crude.Rate.Lower.95..Confidence.Interval[is.na(cdc_deaths$Crude.Rate)], na.rm = TRUE)) # not all rates are suppressed because they're too low... 
 
 
 # next steps:
