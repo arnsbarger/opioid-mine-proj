d65a21ed09031e3c50b10deb2ff4e1fffa125e04
Madison Arnsbarger <maddieka@vt.edu>
2020-02-11 09:01:11 -0500

    finished regression of timing on many predictors...

--- 01-t-test-tables.R
+++ 01-t-test-tables.R
@@ -58,4 +58,14 @@ for (i in 2:(ncol(data2)-1)) {
     
 }
 
+data3 <- merge(x = mines_tables, y = data2, by = "County.Code", all.x = TRUE)
+data3 <- data3 %>% filter(CURRENT_MINE_STATUS %in% c("Abandoned","Abandoned and Sealed"))
+data3$distance_from_2010 <- data3$CURRENT_STATUS_DT - as.Date("2010-01-01")
+
+
+data4 <- data3 %>% select(CURRENT_MINE_TYPE, STATE, Crude.Rate.2010, Crude.Rate.2012, Crude.Rate.2014, Crude.Rate.2016, poverty_rateB17001, median_incomeB19013, distance_from_2010)
+
+fit <- lm(as.numeric(distance_from_2010) ~ ., data4)
+summary(fit)
+
 
