dd796908fbf174d24b6c42d1a679084169dbf21e
Madison Arnsbarger <maddieka@vt.edu>
2020-02-12 11:43:39 -0500

    updated raw data input for mine_qtrly

--- 00-clean-msha-closings.R
+++ 00-clean-msha-closings.R
@@ -3,21 +3,28 @@
 # packages
 library(openxlsx)
 library(dplyr)
+library(data.table)
 
 # import data 
-mine_qtrly <- openxlsx::read.xlsx(xlsxFile = "~/Documents/Pitt/Data/msha_mine_quarterly_employment_production/MineQuartelyDB.xlsx", 
-                                  sheet = 1, 
-                                  startRow = 3, 
-                                  colNames = TRUE,
-                                  detectDates = TRUE)
-names(mine_qtrly) <- c("Prod.Year","Prod.Qtr","Mine.ID","SubunitNum","SubunitName","Quarterly.Hrs","Coal.Prod","Avg.Empl.Cnt")
+# mine_qtrly <- openxlsx::read.xlsx(xlsxFile = "~/Documents/Pitt/Data/msha_mine_quarterly_employment_production/MineQuartelyDB.xlsx", 
+#                                   sheet = 1, 
+#                                   startRow = 3, 
+#                                   colNames = TRUE,
+#                                   detectDates = TRUE)
+# names(mine_qtrly) <- c("Prod.Year","Prod.Qtr","Mine.ID","SubunitNum","SubunitName","Quarterly.Hrs","Coal.Prod","Avg.Empl.Cnt")
 
-mine_closings <- mine_qtrly %>% group_by(Mine.ID, SubunitNum) %>% arrange(Prod.Qtr) %>% slice(which.max(rleid(Avg.Empl.Cnt)))  
+mine_qtrly <- read.delim("~/Documents/Pitt/Data/msha_mine_quarterly_employment_production/MinesProdQuarterly.txt",
+                         header = TRUE,
+                         sep = "|")
 
-mine_closings$status <- ifelse(mine_closings$Avg.Empl.Cnt > 0, 0, 1)
-mine_closings$status_name <- ifelse(mine_closings$Avg.Empl.Cnt > 0, "Open", "Closed")
+mine_qtrly$CAL_PROD_QTR <- paste0(mine_qtrly$CAL_YR, mine_qtrly$CAL_QTR)
+mine_qtrly$MINE_ID <- str_pad(mine_qtrly$MINE_ID, 7, pad = "0")
+
+mine_closings <- mine_qtrly %>% group_by(MINE_ID, SUBUNIT_CD) %>% arrange(CAL_PROD_QTR) %>% slice(which.max(rleid(AVG_EMPLOYEE_CNT)))  
+mine_closings$status <- ifelse(mine_closings$AVG_EMPLOYEE_CNT > 0, 0, 1)
+mine_closings$status_name <- ifelse(mine_closings$AVG_EMPLOYEE_CNT > 0, "Open", "Closed")
 
 
-
+# paste0(names(mine_closings[,6:9]), "_CLOSED")
 
 
