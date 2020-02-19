785c5d59ea771879438e0d80fe2f38dfe3df70f8
Madison Arnsbarger <maddieka@vt.edu>
2020-02-18 19:28:07 -0500

    updated results with marriage/education acs tables, and filled in missing crude rates by backing out from lower CI

--- 01-t-test-tables.R
+++ 01-t-test-tables.R
@@ -7,6 +7,7 @@ library(rgdal)
 library(reshape2)
 library(acs)
 library(dplyr)
+library(stargazer)
 options(scipen=999)
 
 # load mines data frame
@@ -47,6 +48,7 @@ mine_data <- merge(x = mine_data, y = vio_info, by = "MINE_ID", all.x = TRUE)
 mine_data <- merge(x = mine_data, y = mine_closings, by = "MINE_ID", all.x = TRUE)
 
 mine_data$avg_coal_prod[is.nan(mine_data$avg_coal_prod)] <- NA
+mine_data[is.na(mine_data)] <- 0
 
 county_data <- mine_data %>% group_by(County.Code) %>% summarise(num_mines = length(unique(MINE_ID)),
                                                                  avg_coal_prod_cnty = mean(avg_coal_prod, na.rm = TRUE),
@@ -67,13 +69,23 @@ setwd("~/git/opioid-mine-proj/")
 source("00-clean-cdc.R")
 
 cdc_na %>% group_by(Year) %>% summarise(sum(!is.na(Crude.Rate))) # no clear jump; run t-test for all years
+cdc_na$est_crude.rate_from_upper <- as.numeric(cdc_na$Crude.Rate.Upper.95..Confidence.Interval) - 1.96*cdc_na$Crude.Rate.Standard.Error
+cdc_na$est_crude.rate_from_lower <- cdc_na$Crude.Rate.Lower.95..Confidence.Interval + 1.96*cdc_na$Crude.Rate.Standard.Error
+cdcna <- cdc_na[,-c(14:17)]
+# estimated crude.rate from lower CI is closer to the reported crude rates, so I'll put this estimate in the t-table too.
 
 test2 <- cdc_na %>% select(County.Code, Year, Crude.Rate)
-cdc_table_data <- reshape2::dcast(data = test2, formula = County.Code ~ Year, value.var = "Crude.Rate", fun.aggregate = mean)[,1:20]
-names(cdc_table_data) <- c("County.Code",paste0("Crude.Rate.",names(cdc_table_data)[-1]))
+cdc_table_data2 <- reshape2::dcast(data = test2, formula = County.Code ~ Year, value.var = "Crude.Rate", fun.aggregate = mean)[,1:20]
+names(cdc_table_data2) <- c("County.Code",paste0("Crude.Rate.",names(cdc_table_data2)[-1]))
+
+test3 <- cdc_na %>% select(County.Code, Year, est_crude.rate_from_lower)
+cdc_table_data3 <- reshape2::dcast(data = test3, formula = County.Code ~ Year, value.var = "est_crude.rate_from_lower", fun.aggregate = mean)[,1:20]
+names(cdc_table_data3) <- c("County.Code",paste0("est_crude.rate_from_lower",names(cdc_table_data3)[-1]))
+
+cdc_table_data <- merge(cdc_table_data2, cdc_table_data3, by = "County.Code")
 
 # acs county characteristics data
-acs <- read.csv("~/Documents/Pitt/Data/acs_output/acs_mine_sample.csv")[,-1]
+acs <- read.csv("~/Documents/Pitt/Data/acs_output/acs_mine_sample.csv")#[,-1]
 acs$fips <- str_pad(acs$fips, 5, pad = "0")
 
 ### MERGE
@@ -90,13 +102,21 @@ data$any_mines_closed <- ifelse(test = data$total_ever_closed > 0, yes = 1, no =
 results <- data.frame(variable = names(data[,2:(ncol(data)-1)]), t_stat = NA, p_value = NA)
 
 for (i in 2:(ncol(data)-1)) {
-    result <- t.test(data[,i] ~ any_mines_closed, data)
+    result <- t.test(data[,i] ~ any_mines_closed, data, var.equal = FALSE)
     
     results$t_stat[i-1] <- result$statistic
     
     results$p_value[i-1] <- result$p.value
     
+    results$mean_0[i-1] <- result$estimate[1]
+    
+    results$mean_1[i-1] <- result$estimate[2]
+    
+    results$degrees_of_freedom[i-1] <- result$parameter
+    
 }
 
-
-
+stargazer(results %>% filter(p_value <= .1) %>% arrange(p_value), summary = FALSE)
+# 250 counties with mines...
+sum(!is.na(data$num_mines)) 
+length(unique(mine_data$County.Code))
