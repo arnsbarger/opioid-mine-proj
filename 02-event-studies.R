0e39f35f048e2003ef8d1e8d2fcecd2149a86a45
Madison Arnsbarger <maddieka@vt.edu>
2020-06-24 14:11:13 -0400

    found better package and instructions for event studies in R

---
+++ 02-event-studies.R
@@ -0,0 +1,7 @@
+library(eventstudies)
+# https://r-forge.r-project.org/scm/viewvc.php/*checkout*/pkg/vignettes/eventstudies.Rnw?revision=339&root=eventstudies
+
+# have a clean data script and source it here...
+# for now, I'm picking up at the end of 01-randomness-tables.R
+
+
